a85d72031e5ea693480618fe94e63352e16be146
yossebastiands <61390006+yossebastiands@users.noreply.github.com>
2024-12-15 20:58:55 +0100

    Add files via upload

---
+++ 0001_HelloWorld/HelloWorld_001.java
@@ -0,0 +1,10 @@
+// FileName : "HelloWorld_001.java".
+
+public class HelloWorld_001 {
+    // Program begins with a call to main() method
+    public static void main(String args[])
+    {  
+          // Prints "Hello World" 
+        System.out.println("Hello World");
+    }
+}
